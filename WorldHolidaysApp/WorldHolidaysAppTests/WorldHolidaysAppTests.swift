edeff8026f52f9564e72656616002710f8610f0d
Serjo Bykov <serjobikov@gmail.com>
2024-11-12 12:41:24 +0300

    addApp

---
+++ WorldHolidaysApp/WorldHolidaysAppTests/WorldHolidaysAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorldHolidaysAppTests.swift
+//  WorldHolidaysAppTests
+//
+//  Created by Sergio Mascarpone on 12.11.24.
+//
+
+import Testing
+@testable import WorldHolidaysApp
+
+struct WorldHolidaysAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

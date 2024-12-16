04470de4188601ff39481e0aea356a8b8f89754d
Serjo Bykov <serjobikov@gmail.com>
2024-12-16 15:41:13 +0300

    updateApp(правки кода)

--- WorldHolidaysApp/WorldHolidaysApp/Preview Content/Sources/Views/HolidayDetailView.swift
+++ WorldHolidaysApp/WorldHolidaysApp/Preview Content/Sources/Views/HolidayDetailView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct HolidayDetailView: View {
     var holiday: Holiday
-    
+
     var body: some View {
         ScrollView {
             VStack(spacing: 16) {
@@ -17,19 +17,19 @@ struct HolidayDetailView: View {
                     .font(.title)
                     .multilineTextAlignment(.center)
                     .padding(.top)
-                
+
                 // Показываем изображение в развернутом формате
                 Image(holiday.imageName)
                     .resizable()
                     .scaledToFit()
                     .frame(height: 300)
                     .cornerRadius(12)
-                
+
                 Text(holiday.description!)
                     .font(.body)
                     .padding(.horizontal)
-                    .lineLimit(nil) // Разрешаем многострочный текст
-                
+                    .lineLimit(nil)  // Разрешаем многострочный текст
+
                 Spacer()
             }
             .padding()
